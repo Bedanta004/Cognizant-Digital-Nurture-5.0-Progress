fa8e440807a0fe924a4f69a6a7b27c791b69404a
Bedanta004 <mahantybedanta@gmail.com>
2026-07-15 23:55:29 +0530

    Spring Security

---
+++ SpringSecurity/src/main/java/com/BedantaM/SpringSecurity/HelloController.java
@@ -0,0 +1,14 @@
+package com.BedantaM.SpringSecurity;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String greet(HttpServletRequest request){
+        return "Welcome Home "+request.getSession().getId();
+    }
+}
